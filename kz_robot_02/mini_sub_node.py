fec79ceeb6cbedf03d25a0bf2b07490e2ea2a5d0
Kevin <kevinzhouyan@gmail.com>
2024-10-30 19:54:10 -0400

    kz robot 02 draft 1

---
+++ kz_robot_02/mini_sub_node.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class MiniSubNode(Node):
+
+    def __init__(self):
+        super().__init__('mini_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'kztopic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+
+def main(args=None):
+    try:
+        with rclpy.init(args=args):
+            mini_subscriber = MiniSubNode()
+
+            rclpy.spin(mini_subscriber)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'kz_robot_02'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kevin',
+    maintainer_email='kevinzhouyan@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'kz_mini_pub_node = kz_robot_02.mini_pub_node:main',
+            'kz_mini_sub_node = kz_robot_02.mini_sub_node:main'
+        ],
+    },
+)
